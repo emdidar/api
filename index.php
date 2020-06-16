41564dc6b67f98f34cfc129ac7a72bac9217bb02
evisionsoftwareltd <evisionsoftwareltd@gmail.com>
2020-06-16 15:41:12 +0600

    link update

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>API</title>
+	</head>
+	<body>
+		<ul>
+			<li><a href="product/read.php">read</a></li>			
+			<li><a href="product/create.php">create</a></li>
+			<li><a href="product/read_one.php?id=60">read_one</a></li>
+			<li><a href="product/update.php">update</a></li>
+			<li><a href="product/delete.php">delete</a></li>
+			<li><a href="product/search.php?s=shirt">search</a></li>
+			<li><a href="product/read_paging.php">read_paging</a></li>
+		</ul>
+	</body>
+
+</html>
